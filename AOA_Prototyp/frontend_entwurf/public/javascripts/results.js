f5603cf25d1d143e5ce865401d729bbc8a4077db
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-12-08 21:26:04 +0100

    Update results.js

--- AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
+++ AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
@@ -1,9 +1,8 @@
 // erstellen einer leaflet Karte mit Europa als Startpunkt und mit OSM als Basiskarte
 var map = L.map("ergebnismap").setView([52, 7.8], 12);
 
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
 }).addTo(map);
 
 //drawcontrol variables
@@ -58,19 +57,32 @@ map.on(L.Draw.Event.CREATED, (e) => {
 });
 
 // prototypisches Einfügen der Prediction auf der Leaflet Karte
+
 var imageUrl = 'http://localhost:8000/tiffmodel',
 imageBounds = [[51.5, 7], [52, 7.5]];
-L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+var predictionimage = L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Anzeigen der hochgeladenen Shapefile
-var shpfile = new L.Shapefile("/uploads/usertrainingsdata.zip");
-shpfile.addTo(map);
+var usershapefile = new L.Shapefile("/uploads/usertrainingsdata.zip")
 
 // Anzeigen des hochgeladenen geopackages
 // Anmerkung: Layer MUSS layer1 heißen
-var polygons = new L.geoPackageFeatureLayer([], {
+var usergeopackage = new L.geoPackageFeatureLayer([], {
      geoPackageUrl: '/uploads/usertrainingspolygone.gpkg',
      layerName: 'layer1',
      style: {color: 'green'}
-}).addTo(map);
+})
 
+
+var baseMaps = {
+    "OpenStreetMap": osm
+};
+
+var overlayMaps = {
+    "Shapefile": usershapefile,
+    "Geopackage": usergeopackage,
+    "Prediction Image": predictionimage
+};
+
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
