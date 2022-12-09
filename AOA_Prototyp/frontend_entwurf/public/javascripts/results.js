af776c57ec5b53860ab013b3ac99aa175c8647e6
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-12-09 13:30:49 +0100

    Update results.js

--- AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
+++ AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
@@ -1,5 +1,8 @@
+
 // erstellen einer leaflet Karte mit Europa als Startpunkt und mit OSM als Basiskarte
-var map = L.map("ergebnismap").setView([52, 7.8], 12);
+
+
+var map = L.map("anwendungsmap").setView([52, 7.8], 12);
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
@@ -13,7 +16,7 @@ var drawControl = new L.Control.Draw({
     circle: false,
     polyline: false,
     circlemarker: false,
-    rectangle: false
+    polygon: false
   },
   edit: {
     featureGroup: drawnItems
@@ -25,56 +28,72 @@ map.addLayer(drawnItems)
 map.addControl(drawControl)
 
 
-/* adding the drawn rectangle to map via event
+// adding the drawn rectangle to map via event
 map.on(L.Draw.Event.CREATED, (e) => {
   var type = e.layerType;
   var layer = e.layer;
-  polygon = layer.toGeoJSON().geometry.coordinates;
+  rectangle = layer.toGeoJSON().geometry.coordinates;
+  console.log(rectangle)
   drawnItems.addLayer(layer);
   map.addLayer(layer);
-})
-*/
 
-// Prototypisches hinzufügen von polygonen und ihrer Einordnung
-map.on(L.Draw.Event.CREATED, (e) => {
-  var type = e.layerType;
-  var layer = e.layer;
-
-  if(type === "polygon"){
-    var drawnItem = layer;
-    var layer = e.layer;
-    polygon = layer.toGeoJSON().geometry.coordinates;
-    console.log("created polygon");
-    var popupString = `
-        <input id="label" label="label" value="" placeholder="Label">
-        <input id="classid" label="classid" value="" placeholder="Classid">
-        <input type="submit" value="Submit">
-    `;
-    drawnItems.addLayer(layer);
-    map.addLayer(layer);
-    layer.bindPopup(popupString).openPopup();
-  }
+map.on("draw:deleted", function (e) {
+  map.removeControl(drawControl);
+  map.addControl(drawControl);
 });
+})
 
-// prototypisches Einfügen der Prediction auf der Leaflet Karte
-
-var imageUrl = 'http://localhost:8000/tiffmodel',
+// geht nicht da .tif statt .png
+var imageUrl = '/uploads/usersentineldata.tif',
 imageBounds = [[51.5, 7], [52, 7.5]];
-
-var predictionimage = L.imageOverlay(imageUrl, imageBounds).addTo(map);
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Anzeigen der hochgeladenen Shapefile
-var usershapefile = new L.Shapefile("/uploads/usertrainingsdata.zip")
+var usershapefile = new L.Shapefile("/uploads/usertrainingsdata.zip", {
+        onEachFeature: function(feature, layer) {
+            if (feature.properties) {
+                layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+                    return k + ": " + feature.properties[k];
+                }).join("<br />"), {
+                    maxHeight: 200
+                });
+            }
+        },
+        style: function(feature) {
+            return {
+                opacity: 1,
+                fillOpacity: 0.7,
+                radius: 6,
+                color: "orange"
+            }
+        }
+      });
 
 // Anzeigen des hochgeladenen geopackages
 // Anmerkung: Layer MUSS layer1 heißen
 var usergeopackage = new L.geoPackageFeatureLayer([], {
      geoPackageUrl: '/uploads/usertrainingspolygone.gpkg',
      layerName: 'layer1',
-     style: {color: 'green'}
-})
+     onEachFeature: function(feature, layer) {
+            if (feature.properties) {
+                layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+                    return k + ": " + feature.properties[k];
+                }).join("<br />"), {
+                    maxHeight: 200
+                });
+            }
+        },
+        style: function(feature) {
+            return {
+                opacity: 1,
+                fillOpacity: 0.7,
+                radius: 6,
+                color: "green"
+            }
+        }
+      });
 
-
+// Layer Control
 var baseMaps = {
     "OpenStreetMap": osm
 };
@@ -82,7 +101,6 @@ var baseMaps = {
 var overlayMaps = {
     "Shapefile": usershapefile,
     "Geopackage": usergeopackage,
-    "Prediction Image": predictionimage
 };
 
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
