4b44ca64abd7ae128f162333fa4c64b8c3250a12
Jonathan Mader <jonathanmader16@gmail.com>
2022-12-14 01:56:09 +0100

    better georaster plugin usage

--- AOA_Prototyp/frontend_entwurf/public/javascripts/application.js
+++ AOA_Prototyp/frontend_entwurf/public/javascripts/application.js
@@ -192,52 +192,52 @@ var geojsondata = new L.GeoJSON.AJAX("/uploads/usertrainingspolygonegjson.geojso
                 }
             },
       });
-      
+
 // hinzufügen des .tif via georaster plugin: https://github.com/GeoTIFF/georaster und https://github.com/GeoTIFF/georaster-layer-for-leaflet
 fetch("/uploads/usersentineldata.tif")
-  .then((response) => response.arrayBuffer())
-  .then((arrayBuffer) => {
-    parseGeoraster(arrayBuffer).then((georaster) => {
-      console.log("georaster:", georaster);
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => {
+      parseGeoraster(arrayBuffer).then((georaster) => {
+        console.log("georaster:", georaster);
 
-      var geotiffdata = new GeoRasterLayer({
-        georaster: georaster,
-         resolution: 256,
-        pixelValuesToColorFn: (values) => {
-          let maxs = georaster.maxs;
+          // https://github.com/GeoTIFF/georaster-layer-for-leaflet-example/blob/master/examples/separated.html#L45
+          const pixelValuesToColorFn = ([red, green, blue]) => {
           let mins = georaster.mins;
-
-           values[0] = Math.round(
-            (255 / (4000 - mins[0])) * (values[0] - mins[0])
-           );
-          values[1] = Math.round(
-            (255 / (4000 - mins[1])) * (values[1] - mins[1])
-           );
-          values[2] = Math.round(
-            (255 / (4000 - mins[2])) * (values[2] - mins[2])
-           );
+          red = Math.round(
+              (255 / (4000 - mins[0])) * (red - mins[0])
+            );
+          green = Math.round(
+              (255 / (4000 - mins[1])) * (green - mins[1])
+            );
+          blue = Math.round(
+              (255 / (4000 - mins[2])) * (blue - mins[2])
+            );
 
           // make sure no values exceed 255
-          values[0] = Math.min(values[0], 255);
-          values[1] = Math.min(values[1], 255);
-           values[2] = Math.min(values[2], 255);
+          red = Math.min(red, 255);
+          green = Math.min(green, 255);
+          blue = Math.min(blue, 255);
 
           // treat all black as no data
-           if (values[0] === 0 && values[1] === 0 && values[2] === 0)
-             return null;
+          if (red === 0 && green === 0 && blue === 0) return null;
 
-          return `rgb(${values[2]}, ${values[1]}, ${values[0]})`;
-        },
-       });
-      // direktes hinzufügen zur Karte
-       geotiffdata.addTo(map);
+          return `rgb(${blue}, ${green}, ${red})`;
+        };
 
-      map.fitBounds(geotiffdata.getBounds());
+        var geotiffdata = new GeoRasterLayer({
+          georaster: georaster,
+          pixelValuesToColorFn,
+          resolution: 512,
+        });
+        // direktes hinzufügen zur Karte
+        geotiffdata.addTo(map);
 
-      // Asynchrones hinzufügen des Layer zur Layerkontrollfunktion von Leaflet
-      layerControl.addOverlay(geotiffdata, 'Satelliten Bild');
-     });
-});
+        map.fitBounds(geotiffdata.getBounds());
+
+        // Asynchrones hinzufügen des Layer zur Layerkontrollfunktion von Leaflet
+        layerControl.addOverlay(geotiffdata, 'Satelliten Bild');
+      });
+    });
 
 // Layer Control
 var baseMaps = {
--- AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
+++ AOA_Prototyp/frontend_entwurf/public/javascripts/results.js
@@ -268,34 +268,34 @@ fetch("/uploads/usersentineldata.tif")
       parseGeoraster(arrayBuffer).then((georaster) => {
         console.log("georaster:", georaster);
 
+          // https://github.com/GeoTIFF/georaster-layer-for-leaflet-example/blob/master/examples/separated.html#L45
+          const pixelValuesToColorFn = ([red, green, blue]) => {
+          let mins = georaster.mins;
+          red = Math.round(
+              (255 / (4000 - mins[0])) * (red - mins[0])
+            );
+          green = Math.round(
+              (255 / (4000 - mins[1])) * (green - mins[1])
+            );
+          blue = Math.round(
+              (255 / (4000 - mins[2])) * (blue - mins[2])
+            );
+
+          // make sure no values exceed 255
+          red = Math.min(red, 255);
+          green = Math.min(green, 255);
+          blue = Math.min(blue, 255);
+
+          // treat all black as no data
+          if (red === 0 && green === 0 && blue === 0) return null;
+
+          return `rgb(${blue}, ${green}, ${red})`;
+        };
+
         var geotiffdata = new GeoRasterLayer({
           georaster: georaster,
-          resolution: 256,
-          pixelValuesToColorFn: (values) => {
-            let maxs = georaster.maxs;
-            let mins = georaster.mins;
-
-            values[0] = Math.round(
-              (255 / (4000 - mins[0])) * (values[0] - mins[0])
-            );
-            values[1] = Math.round(
-              (255 / (4000 - mins[1])) * (values[1] - mins[1])
-            );
-            values[2] = Math.round(
-              (255 / (4000 - mins[2])) * (values[2] - mins[2])
-            );
-
-            // make sure no values exceed 255
-            values[0] = Math.min(values[0], 255);
-            values[1] = Math.min(values[1], 255);
-            values[2] = Math.min(values[2], 255);
-
-            // treat all black as no data
-            if (values[0] === 0 && values[1] === 0 && values[2] === 0)
-              return null;
-
-            return `rgb(${values[2]}, ${values[1]}, ${values[0]})`;
-          },
+          pixelValuesToColorFn,
+          resolution: 512,
         });
         // direktes hinzufügen zur Karte
         geotiffdata.addTo(map);
@@ -306,6 +306,7 @@ fetch("/uploads/usersentineldata.tif")
         layerControl.addOverlay(geotiffdata, 'Satelliten Bild');
       });
     });
+
 
 // Layer Control
 var baseMaps = {
